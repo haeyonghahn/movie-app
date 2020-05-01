718464937ff68b8b0462232e057233a6d6e2172b
haeyong Hahn <youg1322@naver.com>
2020-05-01 20:52:15 +0900

    Component State #1

--- src/App.js
+++ src/App.js
@@ -23,8 +23,13 @@ const movies = [
 
 class App extends Component {
 
-  // Render : componentWillMount() -> render() -> componentDidMount()
-
+  // Render : 
+  // 1. componentWillMount() :
+  // 만약 영화 어플리케이션을 만들면서 will mount를 진행할 때 api에 작업을 요청하는 부분이다.
+  // 2. render() : 
+  // 3. componentDidMount() :
+  // render에서 작업 수행이 완료되면, 데이터 관련된 작업을 진행하는 부분이다.
+   
   // Update : 
   // 1. componentWillReceiveProps() : 컴포넌트가 새로운 props를 받았다는 의미이다.
   // 2. shouldComponentUpdate() : 리액트는 old props, 새로운 props를 살펴본 다음에 이전과 새로운 props가 다르면,
@@ -35,16 +40,24 @@ class App extends Component {
   // 5. componentDidUpdate() :
   // 예를 들어, 이 부분에서 업데이트 이후에는 spinner하던 '로딩 중' 메세지나 아이콘을 숨긴다.
 
-  // 만약 영화 어플리케이션을 만들면서 will mount를 진행할 때
-  // api에 작업을 요청하는 부분이다.
-  componentWillMount() {
-    console.log('will mount');
+  // state를 변경하면 새로운 state와 함께 render가 다시 작동된다.
+  state = {
+    greeting: 'Hello!'
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      // state를 업데이트하려면 setState()를 사용해야 한다.
+      this.setState({
+        greeting: 'Hello again!'
+      });
+    }, 5000);
   }
 
   render() {
-    console.log('did render');
     return (
       <div className="App">
+        {this.state.greeting}
         {movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
@@ -52,10 +65,6 @@ class App extends Component {
     );
   }
 
-  // render에서 작업 수행이 완료되면, 데이터 관련된 작업을 진행하는 부분이다.
-  componentDidMount() {
-    console.log('did mount');
-  }
 }
 
 export default App;
